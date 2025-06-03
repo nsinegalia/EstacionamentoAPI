b7279039ef53052bc0aaa0d51a89e4cd9ef48093
Nicolas Sinegalia <sinegalia.nicolas@gmail.com>
2025-06-03 19:27:19 -0300

    Algumas alteracoes do pacote de Negocios

--- src/estacionamento/negocio/Movimentacao.java
+++ src/estacionamento/negocio/Movimentacao.java
@@ -14,4 +14,44 @@ public class Movimentacao {
     private LocalDateTime dataHoraSaida;
     private double valor;
 
+    public Movimentacao(Veiculo veiculo, LocalDateTime dataEntrada){
+        this.veiculo = veiculo;
+        this.dataHoraEntrada = dataEntrada;
+
+    }
+
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo){
+        this.veiculo = veiculo;
+    }
+
+    public LocalDateTime getDataHoraEntrada() {
+        return dataHoraEntrada;
+    }
+
+    public void setDataHoraEntrada(LocalDateTime dataHoraEntrada) {
+        this.dataHoraEntrada = dataHoraEntrada;
+    }
+
+    public LocalDateTime getDataHoraSaida() {
+        return dataHoraSaida;
+    }
+
+    public void setDataHoraSaida(LocalDateTime dataHoraSaida) {
+        this.dataHoraSaida = dataHoraSaida;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+
 }
--- src/estacionamento/negocio/Vaga.java
+++ src/estacionamento/negocio/Vaga.java
@@ -2,24 +2,57 @@ package estacionamento.negocio;
 
 /**
  * Essa classe representa as informações relativas à vagas do estacionamento ou status de ocupação
- *
  */
 public class Vaga {
 
     public static int totalVagas = 100;
-    private int vagasOcupadas = 0;
+    private static int vagasOcupadas = 0;
 
-
+    private Vaga() {
+    }
 
     /**
      * Verificar a existencia de alguma vaga livre no estacionamento
      *
      * @return true se houver alguma vaga e false se estiver cheio
      */
-    public boolean temVagaLivre(){
-
+    public static boolean temVagaLivre() {
         //TODO Implementar metodo.
         return false;
     }
+
+    /**
+     * Esse metodo vai buscar o status atual das vagas do estacionamento.
+     */
+    public static void inicializarOcupadas() {
+        //TODO Implementar
+    }
+
+    /**
+     * Retorna o número de vagas ocupadas
+     *
+     * @return numero total de vagas ocupadas num determinado instante.
+     */
+    public static int ocupadas() {
+        return Vaga.vagasOcupadas;
+    }
+
+    /**
+     * Metodo para retornar o numero de vagas livres.
+     *
+     * @return numero total de vagas livres num determinado instante.
+     */
+    public static int livres() {
+        return totalVagas - Vaga.vagasOcupadas;
+    }
+
+
+    /**
+     * Esse metodo atualiza o numero de vagas ocupadas após a entrada do veículo
+     */
+    public static void entrou() {
+        Vaga.vagasOcupadas++;
+    }
+
 
 }
