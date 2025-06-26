68620b55fffe739e61cd125ca10281d72be3f0a9
Nicolas Sinegalia <sinegalia.nicolas@gmail.com>
2025-06-26 19:03:46 -0300

    Atualização do DAO

--- src/estacionamento/persistencia/DAOEstacionamento.java
+++ src/estacionamento/persistencia/DAOEstacionamento.java
@@ -1,35 +1,94 @@
 package estacionamento.persistencia;
 
+import estacionamento.controle.EstacionamentoException;
 import estacionamento.negocio.Movimentacao;
+import estacionamento.negocio.Vaga;
+import estacionamento.utilitario.EstacionamentoUtil;
 
 import java.lang.management.MonitorInfo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class DAOEstacionamento {
 
-    public void criar(Movimentacao movimentacao){
-        //TODO Implementar
+    public void criar(Movimentacao movimentacao) throws EstacionamentoException {
+        String cmd1 = EstacionamentoUtil.get("insertMov");
+        String cmd2 = EstacionamentoUtil.get("atualizaVaga");
 
+        Connection conexao = null;
+        try {
+
+
+            conexao.setAutoCommit(false);
+
+            PreparedStatement stmt = conexao.prepareStatement(cmd1);
+
+            stmt.setString(1, movimentacao.getVeiculo().getPlaca());
+            stmt.setString(2, movimentacao.getVeiculo().getMarca());
+            stmt.setString(3, movimentacao.getVeiculo().getModelo());
+            stmt.setString(4, movimentacao.getVeiculo().getCor());
+            stmt.setString(5, EstacionamentoUtil.getDataAsString(movimentacao.getDataHoraEntrada()));
+
+            stmt.execute();
+
+            stmt = conexao.prepareStatement(cmd2);
+            stmt.setInt(1, Vaga.ocupadas() + 1);
+
+            stmt.execute();
+
+            conexao.commit();
+        } catch (SQLException e) {
+            conexao.rollback();
+            throw new EstacionamentoException("Erro ao registrar veiculo.");
+        }catch (SQLException e1){
+            e1.printStackTrace();
+        }
 
     }
 
-    public void atualizar(Movimentacao movimentacao){
+    public void atualizar(Movimentacao movimentacao) {
         //TODO Implementar
     }
 
-    public Movimentacao buscarMovimentacaoAberta(String placa){
+    public Movimentacao buscarMovimentacaoAberta(String placa) {
         //TODO Implementar
         return null;
 
     }
 
-    public List<Movimentacao> consultarMovimentacoes(LocalDateTime data){
+    public List<Movimentacao> consultarMovimentacoes(LocalDateTime data) {
         //TODO Implementar
         return null;
 
     }
 
+    public static Connection getConnection() throws SQLException {
+
+        String url = EstacionamentoUtil.get("url");
+        String usuario = EstacionamentoUtil.get("usuario");
+        String senha = EstacionamentoUtil.get("senha");
+
+        Connection conexao = DriverManager.getConnection(url, usuario, senha);
+
+        return conexao;
+
+    }
+
+    public static void closeConnection(Connection conexao) {
+
+        if (conexao != null) {
+            try {
+                conexao.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
 
 }
--- src/estacionamento/utilitario/EstacionamentoUtil.java
+++ src/estacionamento/utilitario/EstacionamentoUtil.java
@@ -2,12 +2,16 @@ package estacionamento.utilitario;
 
 import estacionamento.negocio.Movimentacao;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * Essa classe representa uma classe de apoio às demais do sistema.
- *
  */
 
 public class EstacionamentoUtil {
@@ -19,13 +23,18 @@ public class EstacionamentoUtil {
      * @param placa
      * @return true se atender o padrão e false se não
      */
-    public static boolean validarPadraoPlaca(String placa){
+    public static boolean validarPadraoPlaca(String placa) {
 
         String padrao = "[A_Z][A_Z][A_Z]-\\d\\d\\d\\d";
         Pattern p = Pattern.compile(padrao);
         Matcher m = p.matcher(placa);
 
         return m.matches();
+
+    }
+
+    public static String getDataAsString(LocalDateTime dataHoraEntrada) {
+
 
     }
 
@@ -36,10 +45,24 @@ public class EstacionamentoUtil {
      * @param movimentacao
      */
 
-    public void calcularPagamento (Movimentacao movimentacao){
+    public void calcularPagamento(Movimentacao movimentacao) {
         //TODO Implementar
 
 
+    }
+
+
+    public static String get(String propriedade) {
+        Properties prop = new Properties();
+        String valor = null;
+        try {
+            prop.load(new FileInputStream("/configuration.txt"));
+            valor = prop.getProperty(propriedade);
+        } catch (IOException e) {
+            assert false : "Configuracao nao carregada";
+        }
+
+        return valor;
     }
 
 
